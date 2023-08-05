eaa68b08de335ed8e2582415c9e35869c93cef6d
Pritish Rajpurohit <workforpritish@gmail.com>
2023-08-05 15:04:20 +0530

    DAY 5

---
+++ DAY5/Challenge5.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int size, K, count = 0;
+
+
+    cout<<"Size : ";
+    cin>>size;
+
+    int arr[size];
+
+    for(int i=0;i<size;i++){
+        cout<<"Elements : ";
+        cin>>arr[i];
+    }
+
+    cout<<"Input : ";
+    cin>>K;
+
+    for(int i=0;i<size;i++){
+        int temp = arr[i];
+        for(int j=i+1;j<size;j++){
+            if(arr[i]+arr[j] == K){
+                count+=1;
+            }
+        }
+    }
+
+    cout<<"Output : "<<count;
+}
