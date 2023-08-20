f34aba8137aebfc6a7a561eb509677d759d02b50
Pritish Rajpurohit <workforpritish@gmail.com>
2023-08-20 22:23:08 +0530

    DAY 20

---
+++ DAY 20/Challenge20.cpp
@@ -0,0 +1,6 @@
+/*
+Day 20 of 30
+Given a sequence of n strings, the task is to check if any two similar words come together and then destroy each other then print the number of words left in the sequence after this pairwise destruction.
+Input : ab aa aa bcd ab
+Output : 3
+*/
